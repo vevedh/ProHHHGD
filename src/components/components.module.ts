c0cd18d61be46feac16a042e4dfac99d8b10725c
Hervé de CHAVIGNY <vevedh@gmail.com>
2018-04-24 15:17:47 -0400

    Update 24/04/2018 avec euros

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { VvchartsComponent } from './vvcharts/vvcharts';
-@NgModule({
-	declarations: [VvchartsComponent],
-	imports: [],
-	exports: [VvchartsComponent]
-})
-export class ComponentsModule {}
